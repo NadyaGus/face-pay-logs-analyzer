cf99ab5d38c5eebfefe1b0b740ac1a0ada100aba
Nadya Gus <nadyagus95@mail.ru>
2026-07-21 19:17:12 +0300

    rename app

--- src/main/java/com/facepay/main/FacePayLogsAnalyzerApplication.java
+++ src/main/java/com/facepay/main/FacePayLogsAnalyzerApplication.java
@@ -1,13 +1,13 @@
-package com.example.app;
+package com.facepay.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FintechStreamAnalyzerApplication {
+public class FacePayLogsAnalyzerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FintechStreamAnalyzerApplication.class, args);
+		SpringApplication.run(FacePayLogsAnalyzerApplication.class, args);
 	}
 
 }
--- src/test/java/com/facepay/test/FacePayLogsAnalyzerApplicationTests.java
+++ src/test/java/com/facepay/test/FacePayLogsAnalyzerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.app;
+package com.facepay.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FintechStreamAnalyzerApplicationTests {
+class FacePayLogsAnalyzerApplicationTests {
 
 	@Test
 	void contextLoads() {
